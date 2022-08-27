25c2e5c94df0c0931e32e0f51e50d3b95fc271af
NycolasCruz <nycolascruzsilva@gmail.com>
2022-08-27 17:14:25 -0300

    mudança de método post para get

--- routes/auth.php
+++ routes/auth.php
@@ -41,5 +41,5 @@ Route::middleware('auth')->group(function () {
 
     Route::post('confirm-password', [ConfirmablePasswordController::class, 'store']);
 
-    Route::post('logout', [AuthenticatedSessionController::class, 'destroy'])->name('logout');
+    Route::get('logout', [AuthenticatedSessionController::class, 'destroy'])->name('logout');
 });
