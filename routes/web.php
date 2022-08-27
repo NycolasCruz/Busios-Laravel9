f1d0c4fb15bba7c403ca410fe665e5015d812237
NycolasCruz <nycolascruzsilva@gmail.com>
2022-08-27 17:14:06 -0300

    criação de um grupo de rotas

--- routes/web.php
+++ routes/web.php
@@ -17,15 +17,10 @@ Route::get('/', function () {
     return view('auth.register');
 });
 
-
-Route::middleware(['auth'])->group(function (){
+Route::middleware(['auth'])->group(function () {
     Route::get('/dashboard', function () {
         return view('dashboard');
     })->name('dashboard');
-
-
-
 });
-
 
 require __DIR__.'/auth.php';
