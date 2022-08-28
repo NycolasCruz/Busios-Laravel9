a49a7e55de9ed9ee86912c043a8c3f6d619a0eac
NycolasCruz <nycolascruzsilva@gmail.com>
2022-08-28 00:26:23 -0300

    refactor: alteração da rota

--- routes/web.php
+++ routes/web.php
@@ -14,7 +14,7 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('auth.register');
+    return view('auth.login');
 });
 
 Route::middleware(['auth'])->group(function () {
