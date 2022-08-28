7783ad1110c8dd821bb1d5e9135be8b575e9770a
NycolasCruz <nycolascruzsilva@gmail.com>
2022-08-28 00:28:19 -0300

    style: identação

--- resources/views/layouts/main.blade.php
+++ resources/views/layouts/main.blade.php
@@ -1,21 +1,26 @@
 <!doctype html>
 <html lang="pt-br">
-  <head>
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-    <title>Clinic Control</title>
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-gH2yIJqKdNHPEq0n4Mqa/HGKIhSkIHeL5AyhkYV8i59U5AR6csBvApHHNl/vI1Bx" crossorigin="anonymous">
-  </head>
-  <body>
-    
-    @yield('content')
-    @yield('confirm-password')
-    @yield('forgot-password')
-    @yield('login')
-    @yield('register')
-    @yield('reset-password')
-    @yield('verify-email')
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <title>Busios - Gerência de Lojas</title>
 
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0/dist/js/bootstrap.bundle.min.js" integrity="sha384-A3rJD856KowSb7dwlZdYEkO39Gagi7vIsF0jrRAoQmDKKtQBHUuLZ9AsSv4jD4Xa" crossorigin="anonymous"></script>
-  </body>
+        {{-- bootstrap --}}
+        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-gH2yIJqKdNHPEq0n4Mqa/HGKIhSkIHeL5AyhkYV8i59U5AR6csBvApHHNl/vI1Bx" crossorigin="anonymous">
+
+        {{-- favicon --}}
+        <link rel="shortcut icon" href="/favicon.ico" type="image/x-icon">
+    </head>
+    <body>
+        @yield('content')
+        @yield('confirm-password')
+        @yield('forgot-password')
+        @yield('login')
+        @yield('register')
+        @yield('reset-password')
+        @yield('verify-email')
+
+        {{-- bootstrap --}}
+        <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0/dist/js/bootstrap.bundle.min.js" integrity="sha384-A3rJD856KowSb7dwlZdYEkO39Gagi7vIsF0jrRAoQmDKKtQBHUuLZ9AsSv4jD4Xa" crossorigin="anonymous"></script>
+    </body>
 </html>
