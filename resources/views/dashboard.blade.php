0f93def1b6391fa8523324cacd77c45de267c631
NycolasCruz <nycolascruzsilva@gmail.com>
2022-08-29 00:43:32 -0300

    style: table added

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -2,17 +2,67 @@
 @section('content')
 
 <x-app-layout>
-    <x-slot name="header">
-        <h2 class="font-semibold text-xl text-gray-800 leading-tight">
-            {{ __('Dashboard') }}
-        </h2>
-    </x-slot>
+   
 
     <div class="py-12">
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
             <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
                 <div class="p-6 bg-white border-b border-gray-200">
-                    You're logged in!
+                    <div class="p-3">
+
+                        {{-- 
+                            botão de adicionar novas lojas padrao regulação
+
+
+                            nome
+                            logradouro
+                            telefone    
+                            ramo
+                            cpf válido
+                            micro media ou grande empresa(sepearar por cores na tabela)
+                            só eu posso editar minha loja, mas eu posso votar na loja do outro
+
+                            show view    
+                            email do proprietário = auth email
+                            proprietário = auth user
+
+
+                            filtro com laravel
+
+                            menu minha loja
+                            menu ranking de votação
+                        --}}
+                        
+                        <table class="table">
+                            <thead>
+                                <tr>
+                                    <th scope="col">#</th>
+                                    <th scope="col">Nome</th>
+                                    <th scope="col">Proprietário</th>
+                                    <th scope="col">Ramo de Negócio</th>
+                                </tr>
+                            </thead>
+                            <tbody>
+                                <tr>
+                                    <th scope="row">1</th>
+                                    <td>Mark</td>
+                                    <td>Otto</td>
+                                    <td>@mdo</td>
+                                </tr>
+                                <tr>
+                                    <th scope="row">2</th>
+                                    <td>Jacob</td>
+                                    <td>Thornton</td>
+                                    <td>@fat</td>
+                                </tr>
+                                <tr>
+                                    <th scope="row">3</th>
+                                    <td colspan="2">Larry the Bird</td>
+                                    <td>@twitter</td>
+                                </tr>
+                            </tbody>
+                        </table>
+                    </div>  
                 </div>
             </div>
         </div>
