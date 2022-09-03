6af6566ef8c593d0dff402b9e41d665ff7aa4c09
NycolasCruz <nycolascruzsilva@gmail.com>
2022-09-03 14:35:10 -0300

    fix: remove var content

--- resources/views/components/dropdown.blade.php
+++ resources/views/components/dropdown.blade.php
@@ -36,8 +36,5 @@ switch ($width) {
             class="absolute z-50 mt-2 {{ $width }} rounded-md shadow-lg {{ $alignmentClasses }}"
             style="display: none;"
             @click="open = false">
-        <div class="rounded-md ring-1 ring-black ring-opacity-5 {{ $contentClasses }}">
-            {{ $content }}
-        </div>
     </div>
 </div>
