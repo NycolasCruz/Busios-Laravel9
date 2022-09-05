6b60ebccbcb698ebeedfae6d5ca51cd11a1dc148
NycolasCruz <nycolascruzsilva@gmail.com>
2022-09-05 08:46:36 -0300

    refactor: refatoração do controller

--- app/Http/Controllers/StoreController.php
+++ app/Http/Controllers/StoreController.php
@@ -14,16 +14,7 @@ class StoreController extends Controller
 
     public function store(Request $request)
     {
-        $storeCreated = new Store;
-
-        $storeCreated->name = $request->name;
-        $storeCreated->branch = $request->branch;
-        $storeCreated->description = $request->description;
-        $storeCreated->cpf = $request->cpf;
-        $storeCreated->number = $request->number;
-        $storeCreated->place = $request->place;
-
-        return $storeCreated->save();
+        return Store::create($request->all());
     }
 
     public function getAxios()
