24444be5819b5d4e71edff83ca64a20a0b37b062
NycolasCruz <nycolascruzsilva@gmail.com>
2022-09-05 00:58:02 -0300

    feat: add fillable to model

--- app/Models/Store.php
+++ app/Models/Store.php
@@ -8,4 +8,15 @@ use Illuminate\Database\Eloquent\Model;
 class Store extends Model
 {
     use HasFactory;
+
+    protected $table = 'store';
+
+    protected $fillable = [
+        'name',
+        'branch',
+        'description',
+        'cpf',
+        'number',
+        'place'
+    ];
 }
