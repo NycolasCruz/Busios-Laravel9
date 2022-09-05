8bbb37415feb0af50f00f9b63d4c33faec0e39aa
NycolasCruz <nycolascruzsilva@gmail.com>
2022-09-05 00:57:29 -0300

    feat: create and view finished

--- app/Http/Controllers/StoreController.php
+++ app/Http/Controllers/StoreController.php
@@ -3,11 +3,31 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\Store;
 
 class StoreController extends Controller
 {
     public function index()
     {
-        return view('');
+        return view('dashboard');
+    }
+
+    public function store(Request $request)
+    {
+        $storeCreated = new Store;
+
+        $storeCreated->name = $request->name;
+        $storeCreated->branch = $request->branch;
+        $storeCreated->description = $request->description;
+        $storeCreated->cpf = $request->cpf;
+        $storeCreated->number = $request->number;
+        $storeCreated->place = $request->place;
+
+        return $storeCreated->save();
+    }
+
+    public function getAxios()
+    {
+        return Store::all();
     }
 }
