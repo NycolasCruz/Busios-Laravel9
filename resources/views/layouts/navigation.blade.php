0379859c22656568a1655573ce09c8aa8883b007
NycolasCruz <nycolascruzsilva@gmail.com>
2022-09-05 01:01:52 -0300

    style: adjustmens on route name

--- resources/views/layouts/navigation.blade.php
+++ resources/views/layouts/navigation.blade.php
@@ -13,7 +13,7 @@
 
                 <!-- Navigation Links -->
                 <div class="hidden space-x-8 sm:-my-px sm:ml-10 sm:flex">
-                    <x-nav-link :href="route('dashboard')" :active="request()->routeIs('dashboard')">
+                    <x-nav-link :href="route('dashboard.get')" :active="request()->routeIs('dashboard.get')">
                         {{ __('Dashboard') }}
                     </x-nav-link>
                 </div>
