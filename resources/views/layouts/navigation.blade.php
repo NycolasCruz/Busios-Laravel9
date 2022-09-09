1a6eeadc75e3a3ac406ddfa10b107567a87b46af
NycolasCruz <nycolascruzsilva@gmail.com>
2022-09-09 15:52:53 -0300

    style: alteração do name das rotas

--- resources/views/layouts/navigation.blade.php
+++ resources/views/layouts/navigation.blade.php
@@ -13,7 +13,7 @@
 
                 <!-- Navigation Links -->
                 <div class="hidden space-x-8 sm:-my-px sm:ml-10 sm:flex">
-                    <x-nav-link :href="route('dashboard.get')" :active="request()->routeIs('dashboard.get')">
+                    <x-nav-link :href="route('dashboard.index')" :active="request()->routeIs('dashboard.index')">
                         {{ __('Dashboard') }}
                     </x-nav-link>
                 </div>
