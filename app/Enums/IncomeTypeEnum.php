b35bb0fb9ae8a396b2e3235887b99c6d51df552a
NycolasCruz <nycolascruzsilva@gmail.com>
2022-09-11 19:33:53 -0300

    feat: enum finalizado

--- app/Enums/IncomeTypeEnum.php
+++ app/Enums/IncomeTypeEnum.php
@@ -4,9 +4,21 @@ namespace App\Enums;
 
 enum IncomeTypeEnum: int
 {
-    case Salary = 1;
-    case Dividends = 2;
-    case Interest = 3;
-    case Rental = 4;
-    case Other = 5;
+    case LESS_10000 = 1;
+    case BETWEEN_10000_AND_50000 = 2;
+    case MORE_50000 = 3;
+
+    public function getDescription() {
+        return match($this) {
+            IncomeTypeEnum::LESS_10000 => 'Menos que 10.000',
+            IncomeTypeEnum::BETWEEN_10000_AND_50000 => 'Entre 10.000 e 50.000',
+            IncomeTypeEnum::MORE_50000 => 'Mais que 50.000',
+        };
+    }
 }
+
+// documentation
+// IncomeTypeEnum::cases()->value pra pegar todos os valores num loop
+// IncomeTypeEnum::LESS_10000->value pra pegar um valor do caso do enum
+// IncomeTypeEnum::LESS_10000->getDescription() pra pegar a descrição do caso do enum
+// IncoeTypeEnum::tryFrom($value)->getDescription() pra pegar a descrição do caso do enum a partir de um valor
