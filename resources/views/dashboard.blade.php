142c3692fd1df832633deb6abec6890a91401d71
NycolasCruz <nycolascruzsilva@gmail.com>
2022-10-01 17:21:16 -0300

    test

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -9,6 +9,18 @@
                     <div class="p-3">
 
                         {{-- 
+                            dividir arquivos em partes e usar Format Data
+                            dividir arquivos em partes e usar Format Data
+                            dividir arquivos em partes e usar Format Data
+                            dividir arquivos em partes e usar Format Data
+                            dividir arquivos em partes e usar Format Data
+
+
+                            tooltip
+
+
+                            colocar linter no projeto react
+
                             criar um acessor para nome social 
                             barra de pesquisa por nome da loja
                             botão para ver e excluir os currículos pensentes da loja ou enviar um curículo para a loja (many to many) 
@@ -394,6 +406,64 @@
         </div>
     </div>
 </div>
+
+{{-- curriculum modal --}}
+<div class="modal fade" id="curriculum-modal" aria-labelledby="exampleModalLabel" aria-hidden="true">
+    <div class="modal-dialog">
+        <div class="modal-content">
+            <div class="modal-header">
+                <h5 class="modal-title fs-5" id="exampleModalLabel">Visualizar Loja</h5>
+                <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close">
+                    <i class="fa-solid fa-x"></i>
+                </button>
+            </div>
+            <div class="modal-body">
+                <div class="form-floating">
+                    <textarea class="form-control azull" placeholder="Leave a comment here " rows="4" id="floatingTextarea"></textarea>
+                    <label for="floatingTextarea">Comments</label>
+                  </div>
+
+
+
+
+
+
+
+
+
+                  <div class="mb-4">
+                    <label class="block text-gray-700 text-sm font-bold mb-2" for="username">
+                      Username
+                    </label>
+                    <textarea class="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline" id="username" type="text" placeholder="Username" rows="4"></textarea>
+                  </div>
+                  <div class="mb-6">
+                    <label class="block text-gray-700 text-sm font-bold mb-2" for="password">
+                      Password
+                    </label>
+                    <input class="shadow appearance-none border border-red-500 rounded w-full py-2 px-3 text-gray-700 mb-3 leading-tight focus:outline-none focus:shadow-outline" id="password" type="password" placeholder="******************">
+                    <p class="text-red-500 text-xs italic">Please choose a password.</p>
+                  </div>
+                  <div class="flex items-center justify-between">
+                    <button class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline" type="button">
+                      Sign In
+                    </button>
+                    <a class="inline-block align-baseline font-bold text-sm text-blue-500 hover:text-blue-800" href="#">
+                      Forgot Password?
+                    </a>
+                  </div>
+
+
+
+
+                  
+            </div>
+            <div class="modal-footer d-flex justify-content-center">
+                <button type="button" class="btn bg-light text-gray-500" data-bs-dismiss="modal">Fechar</button>
+            </div>
+        </div>
+    </div>
+</div>
 @endsection
 
 @section('js')
@@ -413,7 +483,7 @@
 
         response.data.map((dataStore, i) => {
 
-            let status = {{ Auth::user()->id }} != dataStore.user.id && "d-none"
+            let showActionsToOwner = {{ Auth::user()->id }} != dataStore.user.id && "d-none"
 
             tbody.innerHTML += `
                 <tr class="align-middle">
@@ -432,13 +502,29 @@
                             <i class="fas fa-eye"></i>
                         </button>
                         <button
-                            class="btn bg-warning ms-2 text-white edit-button ${status}"
+                            class="btn bg-dark ms-2 text-white"
+                            title="Enviar Currículo"
+                            data-bs-toggle="modal"
+                            data-bs-target="#curriculum-modal"
+                            data-id="${dataStore.id}"
+                        >
+                            <i class="fa-regular fa-file-lines"></i>
+                        </button>
+                        <button
+                            class="btn bg-warning ms-2 text-white ${showActionsToOwner}"
                             title="Editar Informações"
                             data-bs-toggle="modal"
                             data-bs-target="#edit-modal"
                             data-id="${dataStore.id}"
                         >
                             <i class="fas fa-edit"></i>
+                        </button>
+                        <button
+                            class="btn bg-danger ms-2 text-white ${showActionsToOwner}"
+                            title="excluir Informações"
+                            data-id="${dataStore.id}"
+                        >
+                            <i class="fas fa-trash"></i>
                         </button>
                     </td>
                 </tr>
