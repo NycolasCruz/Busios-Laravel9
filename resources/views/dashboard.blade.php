362511bfbde62976952154b260d24b5c089e21b8
NycolasCruz <nycolascruzsilva@gmail.com>
2022-10-12 23:51:02 -0300

    style: add search input

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -9,17 +9,12 @@
                     <div class="p-3">
 
                         {{-- 
-                            usar Format Data
-                            criar um acessor para nome social 
-                            barra de pesquisa por nome da loja
-                            botão para ver os currículos pensentes da loja ou enviar um curículo para a loja (many to many) 
-                            o proprietário poderá excluir a loja
-
+                            criar um acessor para nome social
                             máscaras
                             fazer arquivo request para a edição
                             exibir erros da request em toastrs
                             exibir os campos income e extras no formulário de edição
-                            formatar extras na exibição dos dados
+                            formatar características na exibição dos dados
                             limpar campos dos formulários ao fechar o modal e remover fechamento do modl ao submitar o formulário, fazendo-o apenas quando o formulário for submetido com sucesso
                             validar cpf
                         --}}
@@ -29,7 +24,19 @@
                                 <i class="fas fa-cart-shopping text-primary fs-1"></i>
                                 <h1 class="ms-4 fs-1">Lojas</h2>
                             </div>
-                            <div>
+                            <div class="d-flex justify-content-lg-end">
+                                <div class="card-title pe-3">
+                                    <div class="d-flex align-items-center position-relative">
+                                        <span class="svg-icon svg-icon-1 position-absolute text-gray ms-4">
+                                            <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none">
+                                                <rect opacity="0.5" x="17.0365" y="15.1223" width="8.15546" height="2" rx="1" transform="rotate(45 17.0365 15.1223)" fill="currentColor"></rect>
+                                                <path d="M11 19C6.55556 19 3 15.4444 3 11C3 6.55556 6.55556 3 11 3C15.4444 3 19 6.55556 19 11C19 15.4444 15.4444 19 11 19ZM11 5C7.53333 5 5 7.53333 5 11C5 14.4667 7.53333 17 11 17C14.4667 17 17 14.4667 17 11C17 7.53333 14.4667 5 11 5Z" fill="currentColor"></path>
+                                            </svg>
+                                        </span>
+                                        <input type="text" class="form-control form-control-solid border-1 ps-15" data-kt-filter="search" placeholder="Pesquisar Loja">
+                                    </div>
+                                </div>
+                               
                                 <button
                                     type="button"
                                     class="btn bg-primary text-white"
