95720ce1db689bfcb6d09d06777538498d0be7aa
NycolasCruz <nycolascruzsilva@gmail.com>
2022-10-16 01:32:09 -0300

    feat: action store curriculum

--- app/Http/Controllers/CurriculumController.php
+++ app/Http/Controllers/CurriculumController.php
@@ -2,9 +2,27 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Curriculum;
 use Illuminate\Http\Request;
 
 class CurriculumController extends Controller
 {
-    //
+    protected Curriculum $repository;
+
+    public function __construct(Curriculum $model)
+    {
+        $this->repository = $model;
+    }
+
+    public function store(Request $request)
+    {
+        $data = $request->only('curriculum');
+        $user = auth()->user()->id;
+        $shop = $request->shop_id;
+
+        $data['user_id'] = $user;
+        $data['shop_id'] = $shop;
+
+        return $this->repository->create($data);
+    }
 }
