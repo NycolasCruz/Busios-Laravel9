c456f3e666c49543b656df4eb3b30899504ab36d
NycolasCruz <nycolascruzsilva@gmail.com>
2023-04-24 15:34:14 -0300

    style: small adjustments

--- resources/views/layouts/navigation.blade.php
+++ resources/views/layouts/navigation.blade.php
@@ -7,25 +7,33 @@
         <div class="flex justify-between h-16">
             <div class="flex">
                 <!-- Logo -->
-                    <a href="/" class="d-flex align-items-center fs-1 fw-bold" style="color: #8181ff;">
-                        BUSIOS
-                    </a>
+                <a href="/" class="d-flex align-items-center fs-1 fw-bold" style="color: #8181ff;">
+                    BUSIOS
+                </a>
 
                 <!-- Navigation Links -->
-                <div class="hidden space-x-8 sm:-my-px sm:ml-10 sm:flex">
-                    <x-nav-link :href="route('dashboard.index')" :active="request()->routeIs('dashboard.index')">
+                <div class="flex" style="margin-left: 2.5rem">
+                    <x-nav-link
+                        :href="route('dashboard.index')"
+                        :active="request()->routeIs('dashboard.index')"
+                    >
                         Dashboard
                     </x-nav-link>
                 </div>
             </div>
 
             <!-- Settings Dropdown -->
-            <div class="hidden sm:flex sm:items-center sm:ml-6">
+            <div class="flex items-center ml-6">
                 <x-dropdown align="right" width="48">
                     <x-slot name="trigger">
-                        <button class="flex items-center text-sm font-medium text-gray-500 hover:text-gray-700 hover:border-gray-300 focus:outline-none focus:text-gray-700 focus:border-gray-300 transition duration-150 ease-in-out dropdown-toggle" data-bs-toggle="dropdown" aria-expanded="false">
+                        <button
+                            class="flex items-center text-sm font-medium text-gray-500 hover:text-gray-700 hover:border-gray-300 focus:outline-none focus:text-gray-700 focus:border-gray-300 transition duration-150 ease-in-out dropdown-toggle"
+                            data-bs-toggle="dropdown"
+                            aria-expanded="false"
+                        >
                             <p class="pe-1">{{ Auth::user()->name }}</p>
                         </button>
+
                         <ul class="dropdown-menu">
                             <li>
                                 <a class="dropdown-item" href="{{ route('logout') }}">
