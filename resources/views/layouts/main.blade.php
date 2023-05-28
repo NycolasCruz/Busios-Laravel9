d406975958d12fa0713f2344c6a714287992a5be
Nycolas Cruz <nycolascruzsilva@gmail.com>
2023-05-28 18:28:24 -0300

    style: small adjustments

--- resources/views/layouts/main.blade.php
+++ resources/views/layouts/main.blade.php
@@ -8,14 +8,23 @@
         {{-- bootstrap --}}
         <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-gH2yIJqKdNHPEq0n4Mqa/HGKIhSkIHeL5AyhkYV8i59U5AR6csBvApHHNl/vI1Bx" crossorigin="anonymous">
 
-        {{-- tailwind --}}
-        <script src="https://cdn.tailwindcss.com"></script>
-
         {{-- favicon --}}
         <link rel="shortcut icon" href="/favicon.ico" type="image/x-icon">
 
         {{-- css --}}
         <link rel="stylesheet" href="/css/styles.css">
+    </head>
+
+    <body>
+        @yield('dashboard')
+        @yield('login')
+        @yield('register')
+
+        {{-- bootstrap --}}
+        <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0/dist/js/bootstrap.bundle.min.js" integrity="sha384-A3rJD856KowSb7dwlZdYEkO39Gagi7vIsF0jrRAoQmDKKtQBHUuLZ9AsSv4jD4Xa" crossorigin="anonymous"></script>
+
+        {{-- tailwind --}}
+        <script src="https://cdn.tailwindcss.com"></script>
 
         {{-- fontawesome --}}
         <script src="https://kit.fontawesome.com/36eec2ffe7.js" crossorigin="anonymous"></script>
@@ -25,17 +34,8 @@
 
         {{-- sweet alert --}}
         <script src="//cdn.jsdelivr.net/npm/sweetalert2@11"></script>
-    </head>
-    <body>
-        @yield('dashboard')
-        @yield('login')
-        @yield('register')
-        @yield('reset-password')
 
-        {{-- bootstrap --}}
-        <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0/dist/js/bootstrap.bundle.min.js" integrity="sha384-A3rJD856KowSb7dwlZdYEkO39Gagi7vIsF0jrRAoQmDKKtQBHUuLZ9AsSv4jD4Xa" crossorigin="anonymous"></script>
-
-        {{-- js --}}
+        {{-- Toast --}}
         <script>
             const Toast = Swal.mixin({
                 toast: true,
